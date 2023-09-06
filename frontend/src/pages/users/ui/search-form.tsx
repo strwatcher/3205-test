0c0098e73832c5117c77e9bced7e96219ced4940
strwatcher <starwatcher.focused@gmail.com>
2023-09-07 00:01:34 +1000

    add mask to number input

--- frontend/src/pages/users/ui/search-form.tsx
+++ frontend/src/pages/users/ui/search-form.tsx
@@ -4,15 +4,18 @@ import { Stack } from "@/shared/ui/stack";
 import { Group } from "@/shared/ui/group";
 import { Input } from "@/shared/ui/input";
 import { Button } from "@/shared/ui/button";
+import { useMaskito } from "@maskito/react";
+import { numberMask } from "@/shared/lib/masks";
 
 export const SearchForm = () => {
   const form = useSearchForm();
+  const numberRef = useMaskito({ options: numberMask });
   return (
     <Form onSubmit={form.onSubmit}>
       <Stack>
         <Group grow>
           <Input label="Email" {...form.fields.email} />
-          <Input label="Номер" {...form.fields.number} />
+          <Input label="Номер" {...form.fields.number} ref={numberRef} />
         </Group>
         <Button type="submit">Найти</Button>
       </Stack>
