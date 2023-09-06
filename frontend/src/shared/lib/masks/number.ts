19e9698f7e0b073db6ba3c8f9206f6a5cd205210
strwatcher <starwatcher.focused@gmail.com>
2023-09-07 00:01:10 +1000

    add number mask and wrap input component in forward ref

---
+++ frontend/src/shared/lib/masks/number.ts
@@ -0,0 +1,5 @@
+import { MaskitoOptions } from "@maskito/core";
+
+export const numberMask: MaskitoOptions = {
+  mask: [/\d/, /\d/, "-", /\d/, /\d/, "-", /\d/, /\d/],
+};
--- frontend/src/shared/ui/input/index.tsx
+++ frontend/src/shared/ui/input/index.tsx
@@ -1,6 +1,7 @@
 import { useId } from "react";
 import s from "./input.module.sass";
 import { Show } from "@/shared/lib/show";
+import React from "react";
 
 export type InputProps = {
   onChange: (value: string) => void;
@@ -9,24 +10,27 @@ export type InputProps = {
   label: string;
 };
 
-export const Input = (props: InputProps) => {
-  const id = useId();
-  const inputId = `input_${id}`;
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (props: InputProps, ref) => {
+    const id = useId();
+    const inputId = `input_${id}`;
 
-  return (
-    <div className={s.wrapper}>
-      <label className={s.label} htmlFor={inputId}>
-        {props.label}
-      </label>
-      <input
-        id={inputId}
-        className={s.input}
-        value={props.value}
-        onChange={(e) => props.onChange(e.target.value)}
-      />
-      <Show when={props.error}>
-        <span className={s.error}>{props.error}</span>
-      </Show>
-    </div>
-  );
-};
+    return (
+      <div className={s.wrapper}>
+        <label className={s.label} htmlFor={inputId}>
+          {props.label}
+        </label>
+        <input
+          ref={ref}
+          id={inputId}
+          className={s.input}
+          value={props.value}
+          onChange={(e) => props.onChange(e.target.value)}
+        />
+        <Show when={props.error}>
+          <span className={s.error}>{props.error}</span>
+        </Show>
+      </div>
+    );
+  },
+);
